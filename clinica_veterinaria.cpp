da0b9900741cf3110aa883b6d888e8960d911480
Clara Joaquim <2411550061@fema.edu.br~>
2025-05-01 19:55:58 -0300

    feat: implementar contexto de inserir e sair do menu de opcoes

--- clinica_veterinaria.cpp
+++ clinica_veterinaria.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <clocale>
+#include <conio.h>
 #define T 3
 
 using namespace std;
@@ -46,7 +48,22 @@ struct consulta {
     float valorConsulta;
 };
 
+void leituraCidades(struct cidade cidades[], int &contCidadeS);
+void leituraRacas(struct raca r[], int &contRacaS);
+void leituraAnimais(struct animal a[], int &contAnimalS);
+void leituraTutores(struct tutor t[], int &contTutorS);
+void leituraVeterinarios(struct veterinario v[], int &contVeterinarioS);
+void leituraConsultas(struct consulta c[], int &contConsultaS);
+char menu();
+void inserir(struct cidade cidades[], int &contCidadeS, struct raca racas[], int &contRacaS,
+             struct animal animais[], int &contAnimalS, struct tutor tutores[], int &contTutorS,
+             struct veterinario veterinarios[], int &contVeterinarioS, struct consulta consultas[],
+             int &contConsultaS);
+char sair();
+
 int main () {
+
+    setlocale(LC_ALL, "Portuguese");
 
     // Definição dos vetores
     cidade cidades[T];
@@ -63,14 +80,127 @@ int main () {
     int contTutorS, contTutorT, contTutorA;
     int contVeterinarioS;
     int contConsultaS;
+
+    //Menu
+    char opcao = 'N';
+    while (opcao != 'S') {
+        opcao = menu();
+        switch (opcao) {
+            case '1': {
+                inserir(cidades, contCidadeS, racas, contRacaS, animais, contAnimalS, tutores, contTutorS,
+                        veterinarios, contVeterinarioS, consultas, contConsultaS);
+                break;
+            }
+                //		  case '2' : { inserirFim(); break; }
+                //		  case '3' : { removerK(); break; }
+            case '4': {
+                opcao = sair();
+                break;
+            }
+            default: {
+                cout << "\n\nOpcao invalida. Tecle algo para continuar...";
+                getch();
+            }
+        }
+    }
 }
+
+//Funções do Menu
+
+char menu() {
+    char opcao;
+    system("cls");
+    cout << "\t\tOpcoes:\n\n";
+    cout << "\t\t\t1 - Inserir dados\n\n";
+    cout << "\t\t\t2 - Incluir novos dados\n\n";
+    cout << "\t\t\t3 - Consultar dados\n\n";
+    cout << "\t\t\t4 - Sair do programa\n\n";
+    cout << "\t\tEscolha a opcao: ";
+    cin >> opcao;
+
+    return opcao;
+}
+
+char sair() {
+    char sair = 'N';
+    cout << "\n\nDeseja encerrar o programa? (S/N) : ";
+    cin >> sair;
+    if (sair == 'S') {
+        cout << "\n\nSaindo do programa...";
+        getch();
+        system("cls");
+    }
+    return sair;
+}
+
+void inserir(struct cidade cidades[], int &contCidadeS, struct raca racas[], int &contRacaS,
+             struct animal animais[], int &contAnimalS, struct tutor tutores[], int &contTutorS,
+             struct veterinario veterinarios[], int &contVeterinarioS, struct consulta consultas[],
+             int &contConsultaS) {
+    char conf;
+
+    char opcao;
+    system("cls");
+    cout << "\t\tDeseja Realizar a Leitura de qual lista?\n\n";
+    cout << "\n\t1 - Cidade" << endl;
+    cout << "\n\t2 - Raça" << endl;
+    cout << "\n\t3 - Animal" << endl;
+    cout << "\n\t4 - Tutor" << endl;
+    cout << "\n\t5 - Veterinario" << endl;
+    cout << "\n\t6 - Consulta" << endl;
+    cout << "\n\t7 - Sair" << endl;
+    cout << "\n\tEscolha a opcao: ";
+    cin >> opcao;
+
+    cout << "\n\t. Confirma a operação? (S/N): ";
+    cin >> conf;
+
+
+    while (conf == 'S') {
+        switch (opcao) {
+            case '1': {
+                leituraCidades(cidades, contCidadeS);
+                break;
+            }
+            case '2': {
+                leituraRacas(racas, contRacaS);
+                break;
+            }
+            case '3': {
+                leituraAnimais(animais, contAnimalS);
+                break;
+            }
+            case '4': {
+                leituraTutores(tutores, contTutorS);
+                break;
+            }
+            case '5': {
+                leituraVeterinarios(veterinarios, contVeterinarioS);
+                break;
+            }
+            case '6': {
+                leituraConsultas(consultas, contConsultaS);
+                break;
+            }
+            case '7': {
+                opcao = sair();
+                break;
+            }
+            default: {
+                cout << "\n\nOpcao invalida. Tecle algo para continuar...";
+                getch();
+            }
+        }
+    }
+}
+
 
 //Exercicio 1 (Leituras)
 
 void leituraCidades(struct cidade c[], int &contCidadeS) {
     int i = 0;
     for (int saida = 1; i < T && saida != 0; i++) {
-        cout << "\nDigite as informações da " << (i + 1) << "º cidade" << endl;
+        cout << "\nDigite as informações da " << (i + 1) << "º Cidade" << endl;
         cout << "Codigo: ";
         cin >> c[i].codigo;
         cin.ignore();
@@ -90,7 +220,7 @@ void leituraCidades(struct cidade c[], int &contCidadeS) {
 void leituraRacas(struct raca r[], int &contRacaS) {
     int i = 0;
     for (int saida = 1; i < T && saida != 0; i++) {
-        cout << "\nDigite as informações da " << (i + 1) << "º raça" << endl;
+        cout << "\nDigite as informações da " << (i + 1) << "º Raça" << endl;
         cout << "Codigo: ";
         cin >> r[i].codigo;
         cin.ignore();
@@ -108,7 +238,7 @@ void leituraRacas(struct raca r[], int &contRacaS) {
 void leituraAnimais(struct animal a[], int &contAnimalS) {
     int i = 0;
     for (int saida = 1; i < T && saida != 0; i++) {
-        cout << "\nDigite as informações do " << (i + 1) << "º animal" << endl;
+        cout << "\nDigite as informações do " << (i + 1) << "º Animal" << endl;
         cout << "Codigo: ";
         cin >> a[i].codigo;
         cin.ignore();
@@ -138,7 +268,7 @@ void leituraAnimais(struct animal a[], int &contAnimalS) {
 void leituraTutores(struct tutor t[], int &contTutorS) {
     int i = 0;
     for (int saida = 1; i < T && saida != 0; i++) {
-        cout << "\nDigite as informações do " << (i + 1) << "º tutor" << endl;
+        cout << "\nDigite as informações do " << (i + 1) << "º Tutor" << endl;
         cout << "Codigo: ";
         cin >> t[i].codigo;
         if (t[i].codigo > 0) {
@@ -164,7 +294,7 @@ void leituraTutores(struct tutor t[], int &contTutorS) {
 void leituraVeterinarios(struct veterinario v[], int &contVeterinarioS) {
     int i = 0;
     for (int saida = 1; i < T && saida != 0; i++) {
-        cout << "\nDigite as informações do " << (i + 1) << "º veterinario" << endl;
+        cout << "\nDigite as informações do " << (i + 1) << "º Veterinario" << endl;
         cout << "Codigo: ";
         cin >> v[i].codigo;
         cin.ignore();
@@ -188,7 +318,7 @@ void leituraVeterinarios(struct veterinario v[], int &contVeterinarioS) {
 void leituraConsultas(struct consulta c[], int &contConsultaS) {
     int i = 0;
     for (int saida = 1; i < T && saida != 0; i++) {
-        cout << "\nDigite as informações da " << (i + 1) << "º consulta" << endl;
+        cout << "\nDigite as informações da " << (i + 1) << "º Consulta" << endl;
         cout << "Codigo: ";
         cin >> c[i].codigo;
         cin.ignore();
