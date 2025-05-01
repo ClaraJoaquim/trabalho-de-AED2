4460b69b1f433a633422aa9ba5e89f4859f26e7e
Clara Joaquim <2411550061@fema.edu.br~>
2025-05-01 19:44:45 -0300

    feat: implementar todas as structs referentes as tabelas

--- clinica_veterinaria.cpp
+++ clinica_veterinaria.cpp
@@ -2,5 +2,48 @@
 
 using namespace std;
 
+struct cidade {
+    int codigo;
+    string nome;
+    char uf[3];
+};
+
+struct raca {
+    int codigo;
+    string descricao;
+};
+
+struct animal {
+    int codigo;
+    string nome;
+    int codigoRaca;
+    int idade;
+    float peso;
+    int codigoTutor;
+};
+
+struct tutor {
+    int codigo;
+    string nome;
+    char cpf[14];
+    string endereco;
+    int codigoCidade;
+};
+
+struct veterinario {
+    int codigo;
+    string nome;
+    string endereco;
+    int codigoCidade;
+};
+
+struct consulta {
+    int codigo;
+    int codigoAnimal;
+    int codigoVeterinario;
+    time_t dataConsulta;
+    float valorConsulta;
+};
+
 int main () {
 }
